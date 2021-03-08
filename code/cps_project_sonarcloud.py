4322314e714278bd488c2775e732ee0a047a6d07
mattia <geom.dibattistamattia@gmail.com>
2021-03-08 18:19:51 +0100

    SonarCloud test

--- code/cps_project_sonarcloud.py
+++ code/cps_project_sonarcloud.py
@@ -1,17 +1,15 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[20]:
+# In[ ]:
 
 
 import pandas as pd
 import numpy as np
-import requests as rq
-import json as js
 from matplotlib import pyplot as mp
 
 
-# In[21]:
+# In[78]:
 
 
 #URL used
@@ -19,7 +17,9 @@ URL_NAZIONALE = "https://raw.githubusercontent.com/pcm-dpc/COVID-19/master/dati-
 URL_REGIONALE = "https://raw.githubusercontent.com/pcm-dpc/COVID-19/master/dati-json/dpc-covid19-ita-regioni.json"
 URL_REGIONALE_LATEST = "https://raw.githubusercontent.com/pcm-dpc/COVID-19/master/dati-json/dpc-covid19-ita-regioni-latest.json"
 
-#parameters for graphs
+#parameters for the graphs
+x_label = "giorni dal 24/02"
+y_label = r"$\frac{nuovi\ positivi}{nuovi\ tamponi}$"  
 grid_color = "grey"
 label_color = "white"
 plot_color="green"
@@ -28,11 +28,10 @@ axes_fontsize = 20
 text_pad = 20
 context = {'axes.edgecolor':'grey', 'axes.facecolor':'black',
            'font.family':'sans-serif', 'figure.facecolor':'black', 'figure.edgecolor':'black',
-           'xtick.color':'white', 'ytick.color':'white', 'savefig.transparent':'True',
-           'figure.facecolor':'black'}
+           'xtick.color':'white', 'ytick.color':'white', 'savefig.transparent':'True'}
 
 
-# In[14]:
+# In[79]:
 
 
 '''
@@ -59,7 +58,7 @@ def showBubbleGraph(longitudine_array, altitude_array, new_positives_array):
         mp.show()
 
 
-# In[16]:
+# In[80]:
 
 
 '''
@@ -67,52 +66,34 @@ La funzione crea un dataframe sull'andamento giornaliero delle regioni italiane.
 L'osservabile scelta è quella dei "nuovi_positivi". Il processo termina
 con il garifco a bolle, prodotto dalla funzione "showBubbleGraph()".
 '''
-def function1():
+def bubbleGraph():
     
     #load data
     data = pd.read_json(URL_REGIONALE_LATEST)
-    
-    #calculate all value for the regions
+
+    #filter the dataframe
     dataframe = data.loc[:,['long', 'lat', 'nuovi_positivi']]
 
-    longitudine_array = []
-    altitude_array = []
-    new_positives_array = []
-    len_dataframe = len(dataframe)
-    
-    ################
-    #da rimuovere cilo, passare direttamente il dataframe
-    
-    for i in range(0, len_dataframe):
-        longitudine_array.append(dataframe.long[i])
-        altitude_array.append(dataframe.lat[i])
-        new_positives_array.append(dataframe.nuovi_positivi[i])
-    
-    dataframe["nuovi_positivi"] = new_positives_array
-    #show graph
-    #showBubbleGraph(longitudine_array, altitude_array, dataframe.loc[:,'nuovi_positivi'])
-
-function1()
+    #print and display the dataframe
+    print(dataframe)
+    showBubbleGraph(dataframe.loc[:,'long'], dataframe.loc[:,'lat'], dataframe.loc[:,'nuovi_positivi'])
 
 
-# In[4]:
+# In[81]:
 
 
 '''
 La funzione produce un grafico a linea, prendendo in input i valori
 per l'ascissa e l'ordinata, con le relative label.
 '''
-def showGraph(title, x_axis, y1_axis, y2_axis, x_label, y_label, info_legend1, info_legend2):
-    
-    #################
-    #rifare nomenclatura
+def showGraph(title, x_axis, first_y_axis, second_y_axis, x_label, y_label, firsti_info_legend, second_info_legend):
     
     with mp.rc_context(context):
   
         mp.figure(figsize=(21,9))
         
-        mp.plot(range(0,x_axis), y1_axis, color=plot_color, label=info_legend1)
-        if(not y2_axis.empty): mp.plot(range(0,x_axis), y2_axis, color='blue', label=info_legend2)
+        mp.plot(range(0,x_axis), first_y_axis, color=plot_color, label=firsti_info_legend)
+        if(not second_y_axis.empty): mp.plot(range(0,x_axis), second_y_axis, color='blue', label=second_info_legend)
         
         mp.title(title, color=label_color, pad=text_pad, fontsize=title_fontsize)
         mp.grid(color=grid_color)
@@ -127,7 +108,7 @@ def showGraph(title, x_axis, y1_axis, y2_axis, x_label, y_label, info_legend1, i
         mp.show()        
 
 
-# In[5]:
+# In[82]:
 
 
 '''
@@ -136,77 +117,75 @@ Ogni valore è calcolato sottraendo i temponi del giorno i+1-esimo
 a quelli del giorno i-esimo, per il primo giorno questo calcolo non avviene.
 In input riceve il dataframe e la sua lunghezza.
 '''
-def calculateNewTamponi(dataframe, len_dataframe):
+def calculateNewSwabs(dataframe, len_dataframe):
     
-    new_tamponi = []
+    new_swabs = []
     
     for i in range(0, len_dataframe):
         if(i == 0): 
             previus_value = dataframe.tamponi[i]
-            new_tamponi.append(dataframe.tamponi[i])
+            new_swabs.append(dataframe.tamponi[i])
         else: 
-            nuovi_tamponi = dataframe.tamponi[i] - previus_value
+            difference = dataframe.tamponi[i] - previus_value
             previus_value = dataframe.tamponi[i]
-            new_tamponi.append(nuovi_tamponi)
+            new_swabs.append(difference)
 
-    return new_tamponi
+    return new_swabs
 
 '''
 La funzione ritorna il rapporto tra "nuovi_positivi/nuovi_tamponi".
 In input si hanno: il dataframe, la sua lunghezza e i nuovi tamponi
 precedentemente calcolati.
 '''
-def calculateRatio(dataframe, len_dataframe, new_tamponi):
+def calculateRatio(dataframe, len_dataframe, new_swabs):
     
     ratio = []
     
     for i in range(0, len_dataframe):
-        if new_tamponi[i] == 0:
-            ratio.append(0)
+        if new_swabs[i] == 0: ratio.append(0)
         else:
+            
             #approximate the number
-            approx = round(dataframe.nuovi_positivi[i]/new_tamponi[i], 2)
+            approx = round(dataframe.nuovi_positivi[i]/new_swabs[i], 2)
             ratio.append(approx)
             
     return ratio
 
 
-# In[11]:
+# In[83]:
 
 
 '''
 La funzione crea un dataframe sull'andamento nazionale italiano, contenente il
 rapporto "nuovi_positivi" e "nuovi_tamponi". Il grafico è prodotto da "showGraph()".
+Nota: il picco vicino il valore 300 è in corrispondenza della fine di dicembre,
+in particolare il 26-27-28-29.
 '''
-def function2():
+def nationalTrend():
 
     #load the data
     data = pd.read_json(URL_NAZIONALE)
     
     #pick only "nuovi_positivi" e "tamponi"
     dataframe = data.loc[:,['nuovi_positivi', 'tamponi']]
-    #calculate "nuovi_tamponi"
+    
     len_dataframe = len(dataframe)
-    new_tamponi = calculateNewTamponi(dataframe, len_dataframe)
+    #calculate "nuovi_tamponi"
+    new_swabs = calculateNewTamponi(dataframe, len_dataframe)
 
-    #calculate the rate "nuovi_positivi/nuovi_tamponi"
-    ratio = calculateRatio(dataframe, len_dataframe, new_tamponi)
+    #calculate the ratio "nuovi_positivi/nuovi_tamponi"
+    ratio = calculateRatio(dataframe, len_dataframe, new_swabs)
     
-    #add the ratio column
+    #add the ratio and new swabs columns
     dataframe["rapporto"] = ratio
-    dataframe["nuovi_tamponi"] = new_tamponi
-    
+    dataframe["nuovi_tamponi"] = new_swabs
+        
+    #print and display the dataframe 
     print(dataframe)
-    
-    #display the graph
-    x_label = "giorni dal 24/02"
-    y_label = r"$\frac{nuovi\ positivi}{nuovi\ tamponi}$"    
-    #showGraph("Andamento nazionale", len_dataframe, dataframe.loc[:,'rapporto'], pd.Series(dtype='float64'), x_label, y_label, "Italia", "null")
-    
-function2()
+    showGraph("Andamento nazionale", len_dataframe, dataframe.loc[:,'rapporto'], pd.Series(dtype='float64'), x_label, y_label, "Italia", "null")
 
 
-# In[19]:
+# In[84]:
 
 
 '''
@@ -233,20 +212,22 @@ e "nuovi_tamponi".
 '''
 def calculateNewDataframe(data, region):
     
-    #calculate all value for the region 1
+    #filter the dataframe 
     region_dataframe = data.loc[data.denominazione_regione == region,['nuovi_positivi', 'tamponi']]
     
     #change the index value
     region_dataframe.index = [x for x in range(0, len(region_dataframe.values))]
     len_dataframe = len(region_dataframe)
     
-    new_tamponi_region = calculateNewTamponi(region_dataframe, len_dataframe)
+    #calculate the new swabs
+    new_swabs_region = calculateNewTamponi(region_dataframe, len_dataframe)
     
-    region_ratio = calculateRatio(region_dataframe, len_dataframe, new_tamponi_region)
+    #calculate (nuovi_positivi/nuovi_tamponi)
+    region_ratio = calculateRatio(region_dataframe, len_dataframe, new_swabs_region)
     
-    #add the ratio column
+    #add the ratio and new swabs columns
     region_dataframe["rapporto"] = region_ratio
-    region_dataframe["nuovi_tamponi"] = new_tamponi_region
+    region_dataframe["nuovi_tamponi"] = new_swabs_region
     
     print(region_dataframe)
     return region_dataframe
@@ -254,7 +235,7 @@ def calculateNewDataframe(data, region):
     
 
 
-# In[ ]:
+# In[85]:
 
 
 '''
@@ -263,19 +244,25 @@ tra "nuovi_positivi" e "nuovi_tamponi". Ciò è svolto in "calculateNewDataframe
 In aggiunta viene prodotto un grafico di confronto tra le due regioni
 scelte dall'utente.
 '''
-def function3():
+def regionalTrend():
     
     #load the data
     data = pd.read_json(URL_REGIONALE)
     
-    print("Insert a region (e.g. Lazio)\n")
+    #get the first region from the user
+    print("Insert a region (e.g. Lazio)")
     input_region_first = inputRegion()
-    print("Insert the second region (e.g. Basilicata)\n(Digit null if you don't want)\n")
+    
+    #get the second region from the user
+    print("Insert the second region (e.g. Basilicata)\n(Digit null if you don't want)")
     input_region_second = inputRegion()
     
+    #filter and calculate a new dataframe with columns: 
+    #nuovi_tamponi; rapporto; nuovi_positivi; tamponi
     dataframe_region_first = calculateNewDataframe(data, input_region_first)
 
-    #calculate all values for the region 2
+    #do the same with the second region
+    #if inserted above
     if(input_region_second != "null"): dataframe_region_second = calculateNewDataframe(data, input_region_second)
     else:
 
@@ -283,15 +270,10 @@ def function3():
         dataframe_region_second["rapporto"] = []
 
     #display the graph
-    x_label = "giorni dal 24/02"
-    y_label = r"$\frac{nuovi\ positivi}{nuovi\ tamponi}$"
-    
     showGraph("Andamento regionale", len(dataframe_region_first), dataframe_region_first.loc[:,'rapporto'], dataframe_region_second.loc[:,'rapporto'], x_label, y_label, input_region_first, input_region_second)
-    
-function3()
 
 
-# In[9]:
+# In[86]:
 
 
 '''
@@ -315,22 +297,23 @@ def showMeanGraph(x_axis, y_axis, mean, n):
         #mp.savefig('graph_new_cases.png')
         legend = mp.legend(fontsize=axes_fontsize)
         mp.setp(legend.get_texts(), color=label_color)
+        
         mp.show()
 
 
-# In[9]:
+# In[87]:
 
 
 '''
 La funzione calcola e mostra la media mobile per un certo intervallo di tempo,
 in questo caso sette giorni.
 '''
-def function4():
+def nationalMean():
     
     #load the data
     data = pd.read_json(URL_NAZIONALE)
     
-    #pick only "nuovi_positivi"
+    #pick only "nuovi_positivi" and "data"
     dataframe = data.loc[:,['nuovi_positivi', 'data']]
     len_dataframe = len(dataframe)
     interval_time = 7
@@ -339,13 +322,9 @@ def function4():
     dataframe = dataframe.iloc[len_dataframe - interval_time:len_dataframe]
 
     #select only the column of "nuovi_positivi"
-    #for the mean()
-    latest_new_posites_array = dataframe.loc[:,'nuovi_positivi']
+    #and calculate the mean
+    mean = (dataframe.loc[:,'nuovi_positivi']).mean()        
     
-    #caluclate the mean()
-    #latest_new_posites_array = np.array(latest_new_posites)
-    mean = latest_new_posites_array.mean()
-        
     #pick only day and month from field "data"
     data_new = []
     for i in range(len_dataframe - interval_time, len_dataframe):
@@ -353,12 +332,21 @@ def function4():
         day_month = dataframe.data[i]
         data_new.append(day_month[5:10])
         
-    #adding data_new to dataframe
+    #overload the field "data" with data_new
     dataframe["data"] = data_new
     
+    #display and print the dataframe
+    print(dataframe)
     showMeanGraph(dataframe.loc[:,'data'], dataframe.loc[:,'nuovi_positivi'], mean, interval_time)
-    
-function4()
+
+
+# In[89]:
+
+
+bubbleGraph()
+nationalTrend()
+regionalTrend()
+nationalMean()
 
 
 # In[ ]:
